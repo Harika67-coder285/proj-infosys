64490a547118ac9687a5593e3ac4d269b1c8674f
Harika67-coder285 <tirumalasettyharika4@gmail.com>
2026-01-12 13:35:24 +0530

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -111,7 +111,7 @@ CSRF_TRUSTED_ORIGINS = [
 ]
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'smtp-relay.brevo.com'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
